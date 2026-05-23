63e68e0f3d7a9c36b3ad6b887e0ee2a3f14adc99
hooray804 <profs_35_pointer@icloud.com>
2026-05-24 07:29:32 +0900

    Create antiadblck-sbxh.user.js

---
+++ antiadblck-sbxh.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         suck sbxh
+// @namespace    https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
+// @version      1.0.0
+// @description  더 나은 웹사이트 보기
+// @author       hooray804
+// @match        *://*sbxh*/*
+// @homepage     https://github.com/hooray804/
+// @downloadURL  https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
+// @updateURL    https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
