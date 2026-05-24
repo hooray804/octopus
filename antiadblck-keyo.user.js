e58b9373a8e347b75d3641233db558f06afee163
hooray804 <profs_35_pointer@icloud.com>
2026-05-24 10:00:16 +0900

    Update antiadblck-keyo.user.js

--- antiadblck-keyo.user.js
+++ antiadblck-keyo.user.js
@@ -4,7 +4,7 @@
 // @version      4.0.0
 // @description  더 빠른 웹사이트 이용
 // @author       hooray804
-// @match        *://kio.a*/*
+// @match        *://kio.ac/*
 // @homepage     https://github.com/hooray804/
 // @downloadURL  https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-keyo.user.js
 // @updateURL    https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-keyo.user.js
