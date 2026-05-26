6b0721c7a5e0d6791d9c625c7315297d6ad8d5fe
hooray804 <profs_35_pointer@icloud.com>
2026-05-26 20:05:18 +0900

    Update antiadblck-sbxh.user.js

--- antiadblck-sbxh.user.js
+++ antiadblck-sbxh.user.js
@@ -7,6 +7,8 @@
 // @match        *://sbxh1.com/*
 // @match        *://sbxh2.com/*
 // @match        *://sbxh3.com/*
+// @match        *://sbxh4.com/*
+// @match        *://sbxh5.com/*
 // @homepage     https://github.com/hooray804/
 // @downloadURL  https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
 // @updateURL    https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
