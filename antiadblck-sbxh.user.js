19483f1bfc76eebc770bbcadc75f70fe0ab36a5a
hooray804 <profs_35_pointer@icloud.com>
2026-05-26 22:09:23 +0900

    Update antiadblck-sbxh.user.js

--- antiadblck-sbxh.user.js
+++ antiadblck-sbxh.user.js
@@ -2,7 +2,7 @@
 // @name         suck sbxh
 // @namespace    https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
 // @version      1.8.0
-// @description  더 나은 웹사이트 보기 (비로그인 상태, VPN, 시크릿 모드 등과 함께 사용하세요.)
+// @description  더 나은 웹사이트 보기 (비로그인 , VPN, 시크릿 모드 등과 함께 사용하세요.) (업데이트가 중단됐으며, https://github.com/FilteringDev/newtoki-cracker를 사용하세요.)
 // @author       hooray804
 // @match        *://sbxh1.com/*
 // @match        *://sbxh2.com/*
