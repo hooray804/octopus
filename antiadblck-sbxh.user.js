602537e15ce62a8199bb459444083ce6de19f6c3
hooray804 <profs_35_pointer@icloud.com>
2026-05-26 20:14:25 +0900

    Update antiadblck-sbxh.user.js

--- antiadblck-sbxh.user.js
+++ antiadblck-sbxh.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         suck sbxh
 // @namespace    https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
-// @version      1.6.0
+// @version      1.7.1
 // @description  더 나은 웹사이트 보기 (비로그인 상태, VPN, 시크릿 모드 등과 함께 사용하세요.)
 // @author       hooray804
 // @match        *://sbxh1.com/*
 // @match        *://sbxh2.com/*
 // @match        *://sbxh3.com/*
+// @match        *://sbxh4.com/*
+// @match        *://sbxh5.com/*
 // @homepage     https://github.com/hooray804/
 // @downloadURL  https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
 // @updateURL    https://raw.githubusercontent.com/hooray804/octopus/refs/heads/main/antiadblck-sbxh.user.js
